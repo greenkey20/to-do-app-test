8519b0b6d56ca513d19b3c9a14f1e10bdd64556f
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-12-16 21:30:45 +0900

    PR from Dev branch (#10)
    * Create LICENSE
    
    * fix: update gitignore and test classes

--- src/test/java/com/greenkey/todoapp/ToDoControllerMockTest.java
+++ src/test/java/com/greenkey/todoapp/ToDoControllerMockTest.java
@@ -1,9 +1,13 @@
 package com.greenkey.todoapp;
 
 import com.google.gson.Gson;
+import com.greenkey.todoapp.todo.mapper.ToDoMapperReference;
+import com.greenkey.todoapp.todo.service.ToDoService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 // 2022.12.16(금) 16h45 클래스 생성
@@ -15,4 +19,16 @@ public class ToDoControllerMockTest {
 
     @Autowired
     private Gson gson;
+
+    // 2022.12.16(금) 21h15 이어서 작성
+    @MockBean
+    private ToDoService toDoService;
+
+    @Autowired
+    private ToDoMapperReference toDoMapperReference;
+
+    @Test
+    void postToDoTest() {
+
+    }
 }
--- src/test/java/com/greenkey/todoapp/ToDoServiceMockTest.java
+++ src/test/java/com/greenkey/todoapp/ToDoServiceMockTest.java
@@ -1,4 +1,24 @@
 package com.greenkey.todoapp;
 
+import com.greenkey.todoapp.todo.repository.ToDoRepository;
+import com.greenkey.todoapp.todo.service.ToDoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+// 2022.12.16(금) 21h10 작성 시작
+@ExtendWith(MockitoExtension.class)
 public class ToDoServiceMockTest {
+    @Mock
+    private ToDoRepository toDoRepository;
+
+    @InjectMocks
+    private ToDoService toDoService;
+
+    @Test
+    void createTodoTest() {
+
+    }
 }

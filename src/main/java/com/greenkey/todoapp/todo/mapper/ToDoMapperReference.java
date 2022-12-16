dc4018525ff816a156c41703d66a59c610f084f3
Eunyoung KANG <greenkey20@gmail.com>
2022-12-16 19:40:49 +0900

    feat: add mapper reference

--- src/main/java/com/greenkey/todoapp/todo/mapper/ToDoMapperReference.java
+++ src/main/java/com/greenkey/todoapp/todo/mapper/ToDoMapperReference.java
@@ -1,2 +1,30 @@
-package com.greenkey.todoapp.todo.mapper;public class ToDoMapperReference {
+package com.greenkey.todoapp.todo.mapper;
+
+import com.greenkey.todoapp.todo.dto.ToDoDto;
+import com.greenkey.todoapp.todo.entity.ToDo;
+import com.greenkey.todoapp.todo.repository.ToDoRepository;
+import org.springframework.stereotype.Component;
+
+// 2022.12.16(금) 16h25 동료수강생분 코드 참고해서 추가
+@Component
+public class ToDoMapperReference {
+    private final ToDoRepository toDoRepository;
+
+    public ToDoMapperReference(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
+    public ToDo toDoPostDtoToToDo(ToDoDto.Post requestBody) {
+        if (requestBody == null) {
+            return null;
+        }
+
+        ToDo toDo = ToDo.builder()
+                .title(requestBody.getTitle())
+                .toDoOrder(requestBody.getToDoOrder())
+                .build();
+
+        return toDo;
+    }
+
 }
